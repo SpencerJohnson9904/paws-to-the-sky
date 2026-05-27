40c7c9f3e387cd883e711fd1aab856d99f7ad122
kate-zhou <Katektzhou@gmail.com>
2026-05-26 17:26:01 -0700

    camera movement

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class cameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset = new Vector3(0, 3, -7);
+    public Transform cat;
+    public float distance = 5f;
+    public float height = 2f;
+    public float smoothSpeed = 5f;
 
     void LateUpdate()
-    {
-        if (target == null) return;
-        transform.position = target.position + offset;
-        transform.LookAt(target);
-    }
+{
+    Quaternion targetRotation = Quaternion.LookRotation(cat.forward);
+    transform.rotation = Quaternion.Slerp(
+        transform.rotation,
+        targetRotation,
+        Time.deltaTime * smoothSpeed
+    );
 
+
+    Vector3 desiredPosition = cat.position 
+        - transform.forward * distance 
+        + Vector3.up * height;
+
+    transform.position = Vector3.Lerp(
+        transform.position,
+        desiredPosition,
+        Time.deltaTime * smoothSpeed
+    );
+
+    //transform.LookAt(cat.position + Vector3.up * 1f);
+}
 }
